3156261ced79e9d61832f034fe9560176a172400
Edgar-Roman <59235912+Edgar-Roman@users.noreply.github.com>
2020-01-30 21:06:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ $(function() {
 	// If the study is called with a parameter for redirection, as explained in the documentation, this value is overwritten. 
 	// To the redirect link, the following information will be appended: (1) participant number, (2) condition, (3) username, (4) description submitted by participant. These variables can be extracted from the link, saved as data, and used for linking the Social Media Ostracism paradigm to subsequent tasks and measures. See documentation for more details.
 
-    settings.defaultredirect = 'https://stanforduniversity.qualtrics.com/jfe/form/SV_5AUruSR36hUMffD';
+    settings.defaultredirect = 'https://stanforduniversity.qualtrics.com/jfe/form/SV_0uq1mygbSeaLX6d';
 
 	
 	// **Tasklength**     
